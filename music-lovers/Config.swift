8bed42bb8f7ac631ef361bf70d8aba78b9a5c27f
Christopher Reitz <christopher@reitz.re>
2017-02-12 23:18:17 +0100

    Add empty config to repo

---
+++ music-lovers/Config.swift
@@ -0,0 +1,15 @@
+//
+//  Config.swift
+//  music-lovers
+//
+//  Created by Christopher Reitz on 08/02/2017.
+//  Copyright © 2017 Christopher Reitz. All rights reserved.
+//
+
+struct Config {
+    struct DiscogsApi {
+        static let BaseUrl = "https://api.discogs.com"
+        static let Key = ""
+        static let Secret = ""
+    }
+}
